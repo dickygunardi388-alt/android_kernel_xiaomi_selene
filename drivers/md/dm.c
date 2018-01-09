1ff5b6e0ab4d5331665664e1b2d7b554f9b7b4af
Mike Snitzer <snitzer@redhat.com>
2018-01-08 20:03:04 -0500

    dm: fix incomplete request_queue initialization
    DM is no longer prone to having its request_queue be improperly
    initialized.
    
    Summary of changes:
    
    - defer DM's blk_register_queue() from add_disk()-time until
      dm_setup_md_queue() by using add_disk_no_queue_reg() in alloc_dev().
    
    - dm_setup_md_queue() is updated to fully initialize DM's request_queue
      (_after_ all table loads have occurred and the request_queue's type,
      features and limits are known).
    
    A very welcome side-effect of these changes is DM no longer needs to:
    1) backfill the "mq" sysfs entry (because historically DM didn't
    initialize the request_queue to use blk-mq until _after_
    blk_register_queue() was called via add_disk()).
    2) call elv_register_queue() to get .request_fn request-based DM
    device's "iosched" exposed in syfs.
    
    In addition, blk-mq debugfs support is now made available because
    request-based DM's blk-mq request_queue is now properly initialized
    before dm_setup_md_queue() calls blk_register_queue().
    
    These changes also stave off the need to introduce new DM-specific
    workarounds in block core, e.g. this proposal:
    https://patchwork.kernel.org/patch/10067961/
    
    In the end DM devices should be less unicorn in nature (relative to
    initialization and availability of block core infrastructure provided by
    the request_queue).
    
    Signed-off-by: Mike Snitzer <snitzer@redhat.com>
    Tested-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -1791,7 +1791,7 @@ static struct mapped_device *alloc_dev(int minor)
 		goto bad;
 	md->dax_dev = dax_dev;
 
-	add_disk(md->disk);
+	add_disk_no_queue_reg(md->disk);
 	format_dev_t(md->name, MKDEV(_major, minor));
 
 	md->wq = alloc_workqueue("kdmflush", WQ_MEM_RECLAIM, 0);
@@ -2221,6 +2221,7 @@ static inline void dm_destroy_inline_encryption(struct request_queue *q)
 int dm_setup_md_queue(struct mapped_device *md, struct dm_table *t)
 {
 	int r;
+	struct queue_limits limits;
 	enum dm_queue_mode type = dm_get_md_type(md);
 
 	switch (type) {
@@ -2258,6 +2259,14 @@ int dm_setup_md_queue(struct mapped_device *md, struct dm_table *t)
 		DMERR("Cannot initialize inline encryption");
 		return r;
 	}
+
+	r = dm_calculate_queue_limits(t, &limits);
+	if (r) {
+		DMERR("Cannot calculate initial queue limits");
+		return r;
+	}
+	dm_table_set_restrictions(t, md->queue, &limits);
+	blk_register_queue(md->disk);
 
 	return 0;
 }
