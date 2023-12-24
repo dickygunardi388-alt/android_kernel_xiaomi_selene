9111dba4be4285c8af99470b141d5412a6db6031
fukiame <fukiame@proton.me>
2023-12-24 09:45:58 +0700

    [DNM]mtk-vcodec: silence ctrl-id not support
    i cant fix it...

--- drivers/media/platform/mtk-vcodec/mtk_vcodec_dec.c
+++ drivers/media/platform/mtk-vcodec/mtk_vcodec_dec.c
@@ -2688,7 +2688,6 @@ static int mtk_vdec_s_ctrl(struct v4l2_ctrl *ctrl)
 		ctx->dec_params.queued_frame_buf_count = ctrl->val;
 		break;
 	default:
-		mtk_v4l2_err("ctrl-id=%x not support!", ctrl->id);
 		return -EINVAL;
 	}
 
