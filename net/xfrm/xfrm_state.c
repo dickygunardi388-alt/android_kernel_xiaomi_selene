4fa962d69f081922ebdbd20bd2508f9ba94bea7a
fukiame <fukiame@proton.me>
2025-06-28 22:46:04 +0700

    Revert "[ALPS05254240] xfrm:add more trace log for xfrm HWT"
    This reverts commit 3ecd8c5048c448e5bfe547ad51c3f9274298455f.
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- net/xfrm/xfrm_state.c
+++ net/xfrm/xfrm_state.c
@@ -843,10 +843,13 @@ static ktime_t xfrm_state_t1;
 static ktime_t xfrm_state_t2;
 static u32 spi_dump[32];
 
-static void xfrm_state_print_btrace(struct net *net, unsigned int h, struct xfrm_state *x)
+static void xfrm_state_print_btrace(unsigned long data)
 {
 	ktime_t xfrm_state_deltatime;
 	ktime_t  xfrm_state_duration;
+	int i;
+
+	data = data;
 
 	xfrm_state_deltatime = ktime_sub(xfrm_state_t2, xfrm_state_t1);
 	xfrm_state_duration = //micro-second
@@ -857,12 +860,9 @@ static void xfrm_state_print_btrace(struct net *net, unsigned int h, struct xfrm
 			trace1, trace2, trace3, trace4);
 		pr_info("[mtk_net][xfrm_state] dutation [%d]\n",
 			xfrm_state_duration);
-		pr_info("[mtk_net][xfrm_state] hmask %d lookup x_num %d net %px\n",
-			net->xfrm.state_hmask, net->xfrm.state_num, net);
-		pr_info("[mtk_net][xfrm_state] byspi %px h %d x %px\n",
-			net->xfrm.state_byspi, h, x);
-		if (xfrm_state_duration > 3000000)
-			BUG_ON(1);
+		for (i = 0; i < 32; i++)
+			pr_info("[mtk_net][xfrm_state] spi_dump[%d] : 0x%x\n",
+				i, spi_dump[i]);
 	}
 }
 
@@ -905,7 +905,7 @@ static struct xfrm_state *__xfrm_state_lookup(struct net *net, u32 mark,
 	hlist_for_each_entry_rcu(x, net->xfrm.state_byspi + h, byspi) {
 		trace1++;
 		xfrm_state_t2 = ktime_get();
-		xfrm_state_print_btrace(net, h, x);
+		xfrm_state_print_btrace(1);
 		if (trace1 < 32)
 			spi_dump[trace1] = x->id.spi;
 		if (x->props.family != family ||
