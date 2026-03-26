a360d6e6c775c515b645516407fcbc2971e54ebe
MD. Jadly Bin Salam Shovon <shovon129131@gmail.com>
2026-03-26 15:52:42 +0600

    Implemented SUSFS v2.1.0 [De-Inlined]
    * rework of sus_kstat
    * rework of open_redirect
    * it is still using `inode->i_state` instead of `inode->i_mapping->flags` on offical/gki upstream susfs
    
    Co-Authored-By: sidex15 <24408329+sidex15@users.noreply.github.com>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -101,6 +101,10 @@
 #include "internal.h"
 #include "fd.h"
 
+#ifdef CONFIG_KSU_SUSFS_SUS_MAP
+#include <linux/susfs_def.h>
+#endif
+
 #ifdef CONFIG_TASK_DELAY_ACCT
 #include <linux/delayacct.h>
 #endif
@@ -1686,6 +1690,10 @@ out:
 	return ERR_PTR(error);
 }
 
+#ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
+extern int susfs_open_redirect_spoof_do_proc_readlink(struct inode *inode, char *tmp_buf, int buflen);
+#endif
+
 static int do_proc_readlink(struct path *path, char __user *buffer, int buflen)
 {
 	char *tmp = (char *)__get_free_page(GFP_KERNEL);
@@ -1694,6 +1702,18 @@ static int do_proc_readlink(struct path *path, char __user *buffer, int buflen)
 
 	if (!tmp)
 		return -ENOMEM;
+
+#ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
+	if (PRE_CHECK_OPEN_REDIRECT(path->dentry->d_inode)) {
+		if (!susfs_open_redirect_spoof_do_proc_readlink(path->dentry->d_inode, tmp, buflen)) {
+			len = strlen(tmp);
+			if (copy_to_user(buffer, tmp, len))
+				len = -EFAULT;
+			kfree(tmp);
+			return len;
+		}
+	}
+#endif
 
 	pathname = d_path(path, tmp, PAGE_SIZE);
 	len = PTR_ERR(pathname);
