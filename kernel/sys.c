ecde25f54d4c5eade4cf5633c894e97533c6ff36
MD. Jadly Bin Salam Shovon <shovon129131@gmail.com>
2026-03-26 14:21:37 +0600

    Implemented SUSFS v2.0.0 [De-Inlined]
    Since we're just going for manual hooks anyways (especially non-gki) just use the manual hooks instead...
    
    * sus_mount and others are complied with the upstream susfs kernel branch v5.10
    * susfs try_umount is restored
    * auto features are deprecated so you have to hide your mounts manually
    
    Co-Authored-By: sidex15 <24408329+sidex15@users.noreply.github.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1207,21 +1207,15 @@ static int override_version(struct new_utsname __user *name)
 #endif
 }
 
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+extern void susfs_spoof_uname(struct new_utsname* tmp);
+#endif
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-	if (!strncmp(current->comm, "bpfloader", 9) ||
-	    !strncmp(current->comm, "netbpfload", 10) ||
-	    !strncmp(current->comm, "netd", 4)) {
-		if (current_uid().val == 0) {
-			strcpy(tmp.release, "5.4.186");
-			pr_debug("fake uname: %s/%d release=%s\n",
-				 current->comm, current->pid, tmp.release);
-		}
-	}
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
