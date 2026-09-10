78d7aa456d80c1d45f98a964060e9ec3d2a4d948
dickygunardi388-alt <dickygunardi388@gmail.com>
2026-09-10 12:54:30 +0700

    Update GPU DVFS settings for MT6768/MT6767

--- drivers/misc/mediatek/base/power/mt6765/mtk_gpufreq_core.h
+++ drivers/misc/mediatek/base/power/mt6765/mtk_gpufreq_core.h
@@ -15,83 +15,111 @@
 #define _MT_GPUFREQ_CORE_H_
 
 /**************************************************
- * MT6762M segment_1 : GPU DVFS OPP table Setting
+ * MT6768/MT6767 : GPU DVFS OPP table Setting
  **************************************************/
-#define SEG1_GPU_DVFS_FREQ0			(376000)/* KHz */
+#define SEG_GPU_DVFS_FREQ0			(1100000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ1			(1075000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ2			(1050000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ3			(1025000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ4			(1000000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ5			(975000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ6			(950000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ7			(925000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ8			(900000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ9			(875000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ10			(850000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ11			(823000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ12			(796000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ13			(769000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ14			(743000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ15			(716000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ16			(690000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ17			(663000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ18			(637000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ19			(611000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ20			(586000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ21			(560000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ22			(535000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ23			(509000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ24			(484000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ25			(467000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ26			(450000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ27			(434000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ28			(417000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ29			(400000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ30			(383000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ31			(366000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ32			(349000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ33			(332000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ34			(315000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ35			(299000)	/* KHz */
 
-#define SEG1_GPU_DVFS_VOLT0			(65000)	/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT0		(105000)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT1		(102500)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT2		(100000)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT3		(97500)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT4		(95000)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT5		(92500)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT6		(90000)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT7		(87500)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT8		(85000)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT9		(82500)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT10		(80000)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT11		(79375)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT12		(78125)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT13		(76875)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT14		(75625)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT15		(75000)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT16		(73750)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT17		(72500)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT18		(71250)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT19		(70625)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT20		(70000)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT21		(69375)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT22		(68750)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT23		(68125)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT24		(66875)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT25		(66875)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT26		(66250)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT27		(65625)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT28		(65000)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT29		(64375)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT30		(64375)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT31		(63750)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT32		(63125)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT33		(62500)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT34		(61875)		/* mV x 100 */
+#define SEG_GPU_DVFS_VOLT35		(61250)		/* mV x 100 */
 
-#define SEG1_GPU_DVFS_VSRAM0			(87500)/* mV x 100 */
+#define SEG_GPU_DVFS_VSRAM0		(115000)	/* mV x 100 */
+#define SEG_GPU_DVFS_VSRAM1		(112500)	/* mV x 100 */
+#define SEG_GPU_DVFS_VSRAM2		(110000)	/* mV x 100 */
+#define SEG_GPU_DVFS_VSRAM3		(107500)	/* mV x 100 */
+#define SEG_GPU_DVFS_VSRAM4		(105000)	/* mV x 100 */
+#define SEG_GPU_DVFS_VSRAM5		(102500)	/* mV x 100 */
+#define SEG_GPU_DVFS_VSRAM6		(100000)	/* mV x 100 */
+#define SEG_GPU_DVFS_VSRAM7		(97500)		/* mV x 100 */
+#define SEG_GPU_DVFS_VSRAM8		(95000)		/* mV x 100 */
+#define SEG_GPU_DVFS_VSRAM9		(92500)		/* mV x 100 */
+#define SEG_GPU_DVFS_VSRAM10	(90000)		/* mV x 100 */
+#define SEG_GPU_DVFS_VSRAM11	(89375)		/* mV x 100 */
+#define SEG_GPU_DVFS_VSRAM12	(88125)		/* mV x 100 */
+#define SEG_GPU_DVFS_VSRAM13	(86875)		/* mV x 100 */
+#define SEG_GPU_DVFS_VSRAM14	(85625)		/* mV x 100 */
+#define SEG_GPU_DVFS_VSRAM15	(85000)		/* mV x 100 */
 
-/**************************************************
- * MT6762 segment_2 : GPU DVFS OPP table Setting
- **************************************************/
-#define SEG2_GPU_DVFS_FREQ0			(650000)/* KHz */
-#define SEG2_GPU_DVFS_FREQ1			(500000)/* KHz */
-#define SEG2_GPU_DVFS_FREQ2			(400000)/* KHz */
-
-#define SEG2_GPU_DVFS_VOLT0			(80000)	/* mV x 100 */
-#define SEG2_GPU_DVFS_VOLT1			(70000)	/* mV x 100 */
-#define SEG2_GPU_DVFS_VOLT2			(65000)	/* mV x 100 */
-
-#define SEG2_GPU_DVFS_VSRAM0			(87500)	/* mV x 100 */
-#define SEG2_GPU_DVFS_VSRAM1			(87500)	/* mV x 100 */
-#define SEG2_GPU_DVFS_VSRAM2			(87500)	/* mV x 100 */
-
-/**************************************************
- * MT6765 segment_3 : GPU DVFS OPP table Setting
- **************************************************/
-#define SEG3_GPU_DVFS_FREQ0			(680000)	/* KHz */
-#define SEG3_GPU_DVFS_FREQ1			(500000)	/* KHz */
-#define SEG3_GPU_DVFS_FREQ2			(400000)	/* KHz */
-
-#define SEG3_GPU_DVFS_VOLT0			(80000)		/* mV x 100 */
-#define SEG3_GPU_DVFS_VOLT1			(70000)		/* mV x 100 */
-#define SEG3_GPU_DVFS_VOLT2			(65000)		/* mV x 100 */
-
-#define SEG3_GPU_DVFS_VSRAM0			(87500)		/* mV x 100 */
-#define SEG3_GPU_DVFS_VSRAM1			(87500)		/* mV x 100 */
-#define SEG3_GPU_DVFS_VSRAM2			(87500)		/* mV x 100 */
-
-/**************************************************
- * MT6765T segment_4 : GPU DVFS OPP table Setting
- **************************************************/
-#define SEG4_GPU_DVFS_FREQ0			(730000)	/* KHz */
-#define SEG4_GPU_DVFS_FREQ1			(500000)	/* KHz */
-#define SEG4_GPU_DVFS_FREQ2			(400000)	/* KHz */
-
-#define SEG4_GPU_DVFS_VOLT0			(80000)		/* mV x 100 */
-#define SEG4_GPU_DVFS_VOLT1			(70000)		/* mV x 100 */
-#define SEG4_GPU_DVFS_VOLT2			(65000)		/* mV x 100 */
-
-#define SEG4_GPU_DVFS_VSRAM0			(87500)		/* mV x 100 */
-#define SEG4_GPU_DVFS_VSRAM1			(87500)		/* mV x 100 */
-#define SEG4_GPU_DVFS_VSRAM2			(87500)		/* mV x 100 */
-
-/**************************************************
- * MT6762D segment_5 : GPU DVFS OPP table Setting
- **************************************************/
-#define SEG5_GPU_DVFS_FREQ0			(600000)/* KHz */
-#define SEG5_GPU_DVFS_FREQ1			(500000)/* KHz */
-#define SEG5_GPU_DVFS_FREQ2			(400000)/* KHz */
-
-#define SEG5_GPU_DVFS_VOLT0			(80000)	/* mV x 100 */
-#define SEG5_GPU_DVFS_VOLT1			(70000)	/* mV x 100 */
-#define SEG5_GPU_DVFS_VOLT2			(65000)	/* mV x 100 */
-
-#define SEG5_GPU_DVFS_VSRAM0			(87500)	/* mV x 100 */
-#define SEG5_GPU_DVFS_VSRAM1			(87500)	/* mV x 100 */
-#define SEG5_GPU_DVFS_VSRAM2			(87500)	/* mV x 100 */
+#define FIXED_VSRAM_VOLT			(85000)		/* mV x 100 */
+#define FIXED_VSRAM_VOLT_THSRESHOLD	(75000)		/* mV x 100 */
 
 /**************************************************
  * PMIC Setting
  **************************************************/
-#define VGPU_MAX_VOLT				(SEG3_GPU_DVFS_VOLT0)
-#define VSRAM_GPU_MAX_VOLT			(SEG3_GPU_DVFS_VSRAM0)
-#define DELAY_FACTOR				(625)
-#define PMIC_SRCLKEN_HIGH_TIME_US		(1000)	/* spec is 1(ms) */
-#define BUCK_VARIATION_MAX			(25000)	/* mV x 100 */
-#define BUCK_VARIATION_MIN			(10000)	/* mV x 100 */
+#define VGPU_MAX_VOLT		(SEG_GPU_DVFS_VOLT0)
+#define VSRAM_GPU_MAX_VOLT	(SEG_GPU_DVFS_VSRAM0)
+#define DELAY_FACTOR		(625)
+#define BUCK_DIFF_MAX		(25000)		/* mV x 100 */
+#define BUCK_DIFF_MIN		(10000)		/* mV x 100 */
+#define NUM_OF_OPP_IDX		(36)
 
 /**************************************************
  * efuse Setting
@@ -119,9 +147,7 @@
 #define ROUNDING_VALUE				(5)
 #define DDS_SHIFT				(14)
 #define GPUPLL_FIN				(26)
-#define GPUPLL_CON0				(g_apmixed_base + 0x24C)
-#define GPUPLL_CON1				(g_apmixed_base + 0x250)
-
+#define GPUPLL_CON1				(g_apmixed_base + 0x24C)
 
 /**************************************************
  * Reference Power Setting
@@ -129,25 +155,21 @@
 #define GPU_ACT_REF_POWER			(1285)		/* mW  */
 #define GPU_ACT_REF_FREQ			(900000)	/* KHz */
 #define GPU_ACT_REF_VOLT			(90000)		/* mV x 100 */
-#define GPU_DVFS_PTPOD_DISABLE_VOLT		(65000)		/* mV x 100 */
+#define GPU_DVFS_PTPOD_DISABLE_VOLT	(80000)		/* mV x 100 */
+#define GPU_DVFS_PTPOD_DISABLE_VSRAM_VOLT (90000)
 
 /**************************************************
  * Log Setting
  **************************************************/
-#define GPUFERQ_TAG "[GPU/DVFS]"
-#define gpufreq_perr(fmt, args...)\
-	pr_debug(GPUFERQ_TAG"[ERROR]"fmt, ##args)
-#define gpufreq_pwarn(fmt, args...)\
-	pr_debug(GPUFERQ_TAG"[WARNING]"fmt, ##args)
-#define gpufreq_pr_debug(fmt, args...)\
-	pr_info(GPUFERQ_TAG"[INFO]"fmt, ##args)
-#define gpufreq_pr_debug(fmt, args...)\
-	pr_debug(GPUFERQ_TAG"[DEBUG]"fmt, ##args)
+#define GPUFERQ_TAG				"[GPU/DVFS]"
+#define gpufreq_pr_err(fmt, args...)		pr_err(GPUFERQ_TAG"[ERROR]"fmt, ##args)
+#define gpufreq_pr_warn(fmt, args...)		pr_warn(GPUFERQ_TAG"[WARNING]"fmt, ##args)
+#define gpufreq_pr_info(fmt, args...)		pr_info(GPUFERQ_TAG"[INFO]"fmt, ##args)
+#define gpufreq_pr_debug(fmt, args...)		pr_debug(GPUFERQ_TAG"[DEBUG]"fmt, ##args)
 
 /**************************************************
  * Condition Setting
  **************************************************/
-#define MT_GPUFREQ_OPP_STRESS_TEST
 #define MT_GPUFREQ_STATIC_PWR_READY2USE
 #define MT_GPUFREQ_LOW_BATT_VOLT_PROTECT
 #define MT_GPUFREQ_BATT_PERCENT_PROTECT /* todo: disable it */
@@ -158,21 +180,21 @@
  * Battery Over Current Protect
  **************************************************/
 #ifdef MT_GPUFREQ_BATT_OC_PROTECT
-#define MT_GPUFREQ_BATT_OC_LIMIT_FREQ			(485000)/* KHz */
+#define MT_GPUFREQ_BATT_OC_LIMIT_FREQ		(485000)	/* KHz */
 #endif
 
 /**************************************************
  * Battery Percentage Protect
  **************************************************/
 #ifdef MT_GPUFREQ_BATT_PERCENT_PROTECT
-#define MT_GPUFREQ_BATT_PERCENT_LIMIT_FREQ		(485000)/* KHz */
+#define MT_GPUFREQ_BATT_PERCENT_LIMIT_FREQ	(485000)	/* KHz */
 #endif
 
 /**************************************************
  * Low Battery Volume Protect
  **************************************************/
 #ifdef MT_GPUFREQ_LOW_BATT_VOLT_PROTECT
-#define MT_GPUFREQ_LOW_BATT_VOLT_LIMIT_FREQ		(485000)/* KHz */
+#define MT_GPUFREQ_LOW_BATT_VOLT_LIMIT_FREQ	(485000)	/* KHz */
 #endif
 
 /**************************************************
@@ -180,11 +202,11 @@
  **************************************************/
 #ifdef CONFIG_PROC_FS
 #define PROC_FOPS_RW(name)	\
-static int mt_ ## name ## _proc_open(struct inode *inode, struct file *file)\
-{	\
-	return single_open(file, mt_ ## name ## _proc_show, PDE_DATA(inode));\
-}	\
-	static const struct file_operations mt_ ## name ## _proc_fops = {\
+	static int mt_ ## name ## _proc_open(struct inode *inode, struct file *file)	\
+	{	\
+		return single_open(file, mt_ ## name ## _proc_show, PDE_DATA(inode));	\
+	}	\
+	static const struct file_operations mt_ ## name ## _proc_fops = {	\
 		.owner = THIS_MODULE,	\
 		.open = mt_ ## name ## _proc_open,	\
 		.read = seq_read,	\
@@ -193,10 +215,10 @@ static int mt_ ## name ## _proc_open(struct inode *inode, struct file *file)\
 		.write = mt_ ## name ## _proc_write,	\
 	}
 #define PROC_FOPS_RO(name)	\
-static int mt_ ## name ## _proc_open(struct inode *inode, struct file *file)\
-{	\
-	return single_open(file, mt_ ## name ## _proc_show, PDE_DATA(inode));\
-}	\
+	static int mt_ ## name ## _proc_open(struct inode *inode, struct file *file)	\
+	{	\
+		return single_open(file, mt_ ## name ## _proc_show, PDE_DATA(inode));	\
+	}	\
 	static const struct file_operations mt_ ## name ## _proc_fops =	\
 	{	\
 		.owner = THIS_MODULE,	\
@@ -212,26 +234,26 @@ static int mt_ ## name ## _proc_open(struct inode *inode, struct file *file)\
 /**************************************************
  * Operation Definition
  **************************************************/
-#define VOLT_NORMALIZATION(volt)\
-((volt % 625) ? (volt - (volt % 625) + 625) : volt)
-#define MIN(x, y) (((x) < (y)) ? (x) : (y))
-#define GPUOP(khz, volt, vsram, idx)	\
+#define VOLT_NORMALIZATION(volt)		((volt % 625) ? (volt - (volt % 625) + 625) : volt)
+#ifndef MIN
+#define MIN(x, y)						(((x) < (y)) ? (x) : (y))
+#endif
+#define GPUOP(khz, volt, vsram)	\
 	{	\
 		.gpufreq_khz = khz,	\
 		.gpufreq_volt = volt,	\
 		.gpufreq_vsram = vsram,	\
-		.gpufreq_idx = idx,	\
 	}
 
 /**************************************************
  * Enumerations
  **************************************************/
 enum g_segment_id_enum {
-	MT6762M_SEGMENT = 1,
-	MT6762_SEGMENT,
-	MT6765_SEGMENT,
-	MT6765T_SEGMENT,
-	MT6762D_SEGMENT,
+	MT6767_SEGMENT = 1,
+	MT6768_SEGMENT,
+	MT6769_SEGMENT,
+	MT6769T_SEGMENT,
+	MT6769Z_SEGMENT,
 };
 enum g_post_divider_power_enum  {
 	POST_DIV2 = 1,
@@ -263,31 +285,26 @@ struct g_opp_table_info {
 	unsigned int gpufreq_khz;
 	unsigned int gpufreq_volt;
 	unsigned int gpufreq_vsram;
-	unsigned int gpufreq_idx;
 };
 struct g_clk_info {
-	struct clk *clk_mux;/* main clock for mfg setting*/
-	struct clk *clk_main_parent;/* sub clock for mfg trans mux setting*/
-	struct clk *clk_sub_parent; /* sub clock for mfg trans parent setting*/
-	struct clk *mtcmos_mfg_async;
-	struct clk *mtcmos_mfg;/* dependent on mtcmos_mfg_async */
-	struct clk *mtcmos_mfg_core0;/* dependent on mtcmos_mfg */
+	struct clk *clk_mux;		/* main clock for mfg setting */
+	struct clk *clk_main_parent;	/* substitution clock for mfg transient mux setting */
+	struct clk *clk_sub_parent;	/* substitution clock for mfg transient parent setting */
+	struct clk *subsys_mfg_cg;	/* clock gating */
+	struct clk *mtcmos_mfg_async;	/* */
+	struct clk *mtcmos_mfg;		/* dependent on mtcmos_mfg_async */
+	struct clk *mtcmos_mfg_core0;	/* dependent on mtcmos_mfg */
+	struct clk *mtcmos_mfg_core1;	/* dependent on mtcmos_mfg */
 };
 struct g_pmic_info {
 	struct regulator *reg_vgpu;
 	struct regulator *reg_vsram_gpu;
-	struct regulator *reg_vcore;
-	struct pm_qos_request pm_vgpu;
 };
 
 /**************************************************
  * External functions declaration
  **************************************************/
 extern bool mtk_get_gpu_loading(unsigned int *pLoading);
-extern unsigned int mt_get_ckgen_freq(unsigned int ckgen);
-extern u32 get_devinfo_with_index(u32 index);
-
-extern int (mt_dfs_general_pll)(unsigned int pll_id, unsigned int dds);
-
+extern unsigned int mt_get_ckgen_freq(unsigned int);
 
 #endif /* _MT_GPUFREQ_CORE_H_ */
